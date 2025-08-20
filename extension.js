4747872d234a3d3827e27f47f2b5e0f6a9fc03ff
Jaefae <JackHendrix999@gmail.com>
2025-08-20 12:42:22 -0400

    Refactor job handling logic and enhance delete functionality

--- extension.js
+++ extension.js
@@ -5,11 +5,13 @@ const tableBodyEl = document.getElementById('job-list-body');
 
 let jobs = JSON.parse(localStorage.getItem('jobs'));
 
-if (jobs) {
+if (jobs.length > 0) {
     for(let key in jobs) {
         createJobRow(tableBodyEl,jobs[key]);
     };
-};
+} else {
+    createJobRow(tableBodyEl);
+}
 
 addJobEl.addEventListener('click', () => {
     console.log('Add job button clicked');
--- utils/utils.js
+++ utils/utils.js
@@ -1,35 +1,52 @@
 let jobs = JSON.parse(localStorage.getItem('jobs'));
 
 if (!jobs) {
-    jobs = {};
+    jobs = [];
 }
+
 
 function handleInputChange(e) {
     const field = this.className;
     const rowIdx = this.closest('tr').id;
-
     if (!jobs[rowIdx]) {
         jobs[rowIdx] = {};
     };
 
     jobs[rowIdx][field] = this.value;
-    console.log(`Updated job at index ${rowIdx} field ${field} to ${this.value}`);
     localStorage.setItem('jobs', JSON.stringify(jobs));
 };
 
+function handleDeleteClick(e) {
+    const rowIdx = this.closest('tr').id;
+    console.log('Delete button clicked for row:', rowIdx);
+    jobs.pop(rowIdx);
+    const tableBodyEl = document.getElementById('job-list-body');
+    const rowEl = document.getElementById(rowIdx);
+    
+    if (tableBodyEl.children.length > 1) {
+        rowEl.remove();
+    } else {
+        rowEl.querySelectorAll('input').forEach(input => input.value = null);
+        rowEl.querySelector('select').value = 'Saved';
+    }
+
+    localStorage.setItem('jobs', JSON.stringify(jobs));
+}
+
 export function createJobRow(tableBodyEl,job) {
     const row = document.createElement('tr');
-
     const keys = ['title', 'company', 'salary', 'date', 'status'];
     const jobData = [job?.title, job?.company, job?.salary, job?.date, job?.status];
 
+    row.id = tableBodyEl.children.length;
+
+    // Create input fields for each key
     for (let i = 0; i < jobData.length; i++) {
         const cell = document.createElement('td');
         let input;
         if (i === (jobData.length-1)) {
             input = document.createElement('select');
-            console.log("Creating a select element for status");
-            const options = ['Applied', 'Interview', 'Offer', 'Rejected'];
+            const options = ['Saved','Applied', 'Interview', 'Offer', 'Rejected'];
             options.forEach(option => {
                 const opt = document.createElement('option');
                 opt.value = option;
@@ -44,17 +61,23 @@ export function createJobRow(tableBodyEl,job) {
             input.type = 'text';
             input.value = jobData[i] || '';
         }
-        
-        input.placeholder = keys[i].charAt(0).toUpperCase() + keys[i].slice(1);
 
+        // Set attributes and event listeners
+        input.placeholder = keys[i].charAt(0).toUpperCase() + keys[i].slice(1);
         input.className = keys[i];
-        
-        input.addEventListener('change', handleInputChange);
+        input.addEventListener('input', handleInputChange);
         
         cell.appendChild(input);
         row.appendChild(cell);
     };
+    
 
-    row.id = tableBodyEl.children.length;  
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'x';
+    deleteBtn.className = 'delete-btn-' + row.id;
+    deleteBtn.addEventListener('click', handleDeleteClick);  
+    row.appendChild(deleteBtn);
+
     tableBodyEl.appendChild(row);
 };

7dc4e8261d31b1fdc04768c108d3b414e7938ae3
Jaefae <JackHendrix999@gmail.com>
2025-08-20 12:42:09 -0400

    Refactor job row creation and implement saving

--- extension.js
+++ extension.js
@@ -3,8 +3,16 @@ import { createJobRow }  from './utils/utils.js'
 const addJobEl = document.getElementById('add-job');
 const tableBodyEl = document.getElementById('job-list-body');
 
+let jobs = JSON.parse(localStorage.getItem('jobs'));
+
+if (jobs) {
+    for(let key in jobs) {
+        createJobRow(tableBodyEl,jobs[key]);
+    };
+};
+
 addJobEl.addEventListener('click', () => {
     console.log('Add job button clicked');
     createJobRow(tableBodyEl);
-})
+});
 
--- utils/utils.js
+++ utils/utils.js
@@ -1,33 +1,60 @@
+let jobs = JSON.parse(localStorage.getItem('jobs'));
+
+if (!jobs) {
+    jobs = {};
+}
+
+function handleInputChange(e) {
+    const field = this.className;
+    const rowIdx = this.closest('tr').id;
+
+    if (!jobs[rowIdx]) {
+        jobs[rowIdx] = {};
+    };
+
+    jobs[rowIdx][field] = this.value;
+    console.log(`Updated job at index ${rowIdx} field ${field} to ${this.value}`);
+    localStorage.setItem('jobs', JSON.stringify(jobs));
+};
+
 export function createJobRow(tableBodyEl,job) {
     const row = document.createElement('tr');
-    const jobData = [job?.title, job?.company, job?.salary];
+
+    const keys = ['title', 'company', 'salary', 'date', 'status'];
+    const jobData = [job?.title, job?.company, job?.salary, job?.date, job?.status];
 
     for (let i = 0; i < jobData.length; i++) {
         const cell = document.createElement('td');
-        const input = document.createElement('input');
-        if (i < 3) {
+        let input;
+        if (i === (jobData.length-1)) {
+            input = document.createElement('select');
+            console.log("Creating a select element for status");
+            const options = ['Applied', 'Interview', 'Offer', 'Rejected'];
+            options.forEach(option => {
+                const opt = document.createElement('option');
+                opt.value = option;
+                opt.textContent = option;
+                if (jobData[i] === option) {
+                    opt.selected = true;
+                }
+                input.appendChild(opt);
+            });
+        } else { 
+            input = document.createElement('input'); 
             input.type = 'text';
             input.value = jobData[i] || '';
-        } else if (i === 3) {
-            input.type = 'date';
         }
+        
+        input.placeholder = keys[i].charAt(0).toUpperCase() + keys[i].slice(1);
+
+        input.className = keys[i];
+        
+        input.addEventListener('change', handleInputChange);
+        
         cell.appendChild(input);
         row.appendChild(cell);
-    }
-
-    const date = document.createElement('input');
-    const now = new Date();
-    date.type = 'text';
-    date.value = now.getMonth() + "/" + now.getDate();
-    date.className = "date";
-
-    const status = document.createElement('td');
-    status.textContent = "Applied";
-    status.className = "status";
-
-    row.appendChild(date);
-    row.appendChild(status);
+    };
 
     row.id = tableBodyEl.children.length;  
     tableBodyEl.appendChild(row);
-}
+};
